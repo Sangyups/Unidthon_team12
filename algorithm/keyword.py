f035c502ba1e2902c7253c6a266434f1de2b805c
Jinmo Kim <jmkim1012@postech.ac.kr>
2021-11-06 01:52:49 +0900

    Implemented keyword extraction from texts
    Implemented keyword extractor using konlpy and krwordrank.
    stopwords.txt is updated for more reasonable output.

---
+++ algorithm/keyword.py
@@ -0,0 +1,38 @@
+from krwordrank.word import summarize_with_keywords
+from konlpy.tag import Okt
+
+
+def keywordExtractor(news_list):
+	#* initial settings
+	min_count = 3
+	max_length = 10
+	beta = 0.85
+	max_iter = 10
+	num_keywords = 10
+	okt = Okt()
+	
+	#* preprocessed news list
+	news_corpus = []
+
+	#* load stopwords
+	with open('stopwords.txt') as f:
+		stopwords = f.read().splitlines()
+
+	#* preprocessing (noun extraction)
+	for news in news_list:
+		news_corpus.append(' '.join(okt.nouns(news)))
+
+	#* extract keywords from news text
+	keywords = summarize_with_keywords(news_corpus, num_keywords, stopwords, min_count, max_length, beta, max_iter)
+	print(keywords)
+
+	keywords_list = list(keywords)
+	print(keywords_list)
+	return keywords_list
+
+if __name__ == '__main__':
+	test_list = []
+	with open('articles.txt') as f:
+		test_list.append(f.read())
+
+	keywordExtractor(test_list)
